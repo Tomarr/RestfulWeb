fa14445673b6f36194b5d43f5e0ba62bc6a56179
Rahul <rahul.0187@gmail.com>
2015-12-10 20:47:32 -0600

    Rest

---
+++ Rest/src/main/java/SpringApplication/Application.java
@@ -0,0 +1,17 @@
+package SpringApplication;
+
+/**
+ * Created by Batman on 12/9/2015.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
